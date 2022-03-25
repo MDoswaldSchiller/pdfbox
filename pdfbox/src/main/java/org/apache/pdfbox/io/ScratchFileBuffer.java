4fbd1879dfd9f70e51436b882976590544b139e0
Michael Doswald <michael.doswald@schiller.ch>
2022-03-25 16:22:26 +0100

    Removed finalize() methods All classes offer a way to close the instances. The finalize() methods are just a fallback, if the library is used incorrectly. Objects with finalize() methods are a burden for garbage collectors. The objects need to be queued for finalization which causes the objects to be tenured to the old generation (e.g. in G1). This in turn means that the objects are not cleared on young generation gc cycles which causes full gcs over time.

--- pdfbox/src/main/java/org/apache/pdfbox/io/ScratchFileBuffer.java
+++ pdfbox/src/main/java/org/apache/pdfbox/io/ScratchFileBuffer.java
@@ -497,31 +497,4 @@ class ScratchFileBuffer implements RandomAccess
             size = 0;
         }
     }
-    
-    /**
-     * While calling finalize is normally discouraged we will have to
-     * use it here as long as closing a scratch file buffer is not 
-     * done in every case. Currently {@link COSStream} creates new
-     * buffers without closing the old one - which might still be
-     * used.
-     * 
-     * <p>Enabling debugging one will see if there are still cases
-     * where the buffer is not closed.</p>
-     */
-    @Override
-    protected void finalize() throws Throwable
-    {
-        try
-        {
-            if ((pageHandler != null) && LOG.isDebugEnabled())
-            {
-                LOG.debug("ScratchFileBuffer not closed!");
-            }
-            close();
-        }
-        finally
-        {
-            super.finalize();
-        }
-    }
 }
